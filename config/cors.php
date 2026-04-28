846314725540715020fba9d0d4927545700d53a5
Chirag <dev1@acquaintsoft.com>
2026-04-28 11:17:18 +0530

    Fixed migration issue

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'demo/*', 'demo/upload'],
 
     'allowed_methods' => ['*'],
 
